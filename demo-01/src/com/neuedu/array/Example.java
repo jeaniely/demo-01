9960317c734550b254b23beae1b1bb0ee4edec6f
jeaniely <574156761@qq.com>
2019-09-04 15:10:39 +0800

    选择语句

---
+++ demo-01/src/com/neuedu/array/Example.java
@@ -0,0 +1,5 @@
+package com.neuedu.array;
+
+public class Example {
+
+}
---
+++ demo-01/src/com/neuedu/repeat/Example.java
@@ -0,0 +1,14 @@
+package com.neuedu.repeat;
+public class Example {
+    /**
+     *
+     * 1：输入一个数，判断是否是素数，如果是，则输出“yes” ，
+     * 如果不是，输出“no”
+     *
+     *
+     *
+     * 2：输出100到200之间所有的素数
+     *
+     * **/
+
+}
