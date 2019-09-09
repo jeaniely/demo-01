b7d73c6fcf1f1fa5ea92e1157c807e2928f42b61
jeaniely <574156761@qq.com>
2019-09-09 10:38:40 +0800

    方法概念

---
+++ demo-01/src/com/neuedu/function/Example.java
@@ -0,0 +1,32 @@
+package com.neuedu.function;
+
+public class Example {
+    /**
+     * 方法（函数）：代码的模块化
+     * 1:方法定义
+     *    修饰符  返回类型 方法名(参数){
+     *        方法体
+     *    }
+     *   案例： public int add(int a,int b){
+     *        return a+b;
+     *    }
+     *   修饰符：限定修饰符：public private 默认 protected
+     *            类名：public 默认
+     *            方法|属性：public private 默认 protected
+     *           static
+     *           final
+     *   返回类型： 基本数据类型，引用数据类型，     void
+     *                         return
+     *   参数：形参 ： 数据类型 形参名
+     *
+     * 重载：在一个类中，方法名相同，参数类型不同（参数的个数，参数类型，参数顺序）
+     *        和返回值无关
+     *
+     *
+     * 2：方法的调用
+     *  条件：1）谁调用：对象
+     *        2） 调用什么：方法
+     *        3） 怎么调用： 是否需要传参--实参，是否有返回值
+     *
+     * **/
+}
---
+++ demo-01/src/com/neuedu/function/Test.java
@@ -0,0 +1,9 @@
+package com.neuedu.function;
+
+public class Test {
+    public static void main(String[] args) {
+        Function_01 function_01=new Function_01();
+        double sum=  function_01.add(78.5,12);
+        System.out.println(sum);
+    }
+}
