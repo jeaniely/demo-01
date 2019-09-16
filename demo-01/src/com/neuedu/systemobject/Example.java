ef568316e1778d9dd249cafac9b3862b8ec28d15
jeaniely <574156761@qq.com>
2019-09-16 15:31:12 +0800

    java.lang

---
+++ demo-01/src/com/neuedu/systemobject/Example.java
@@ -0,0 +1,50 @@
+package com.neuedu.systemobject;
+
+public class Example {
+    /**
+     * 1：那些app
+     *    java.lang  默认 常用的包装类、比较、显示等，Object
+     *    java.util 工具包：集合、日期等
+     *    java.io  输入和输出流
+     *    java.math 数学公式
+     *    java.sql jdbc的使用
+     *    。。。。
+     *2： java.lang
+     *     Object   : equals与==
+     *                == ：基本数据类型：两个数值比较是否相等
+     *                     对象：与equals相同，比较是否同一个对象
+     *                equals：只能比较对象，比较是否同一个对象
+     *                        能够被重写
+     *    基本数据类型  包装类
+     *    byte  --  Byte
+     *    short -- Short
+     *    int   --Integer
+     *    long  --Long
+     *    char -- Character
+     *    boolean-- Boolean
+     *    float ---Float
+     *    double --Double
+     *
+     *   String  字符串
+     *
+     *
+     *
+     *
+     *
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ demo-01/src/com/neuedu/systemobject/object01/ObjectExample.java
@@ -0,0 +1,8 @@
+package com.neuedu.systemobject.object01;
+
+public class ObjectExample {
+    public static void main(String[] args) {
+        Object o=new Object();
+        System.out.println(o.hashCode());
+    }
+}
